c5ab7aba5a257c52f3cdf171f8632692962918dd
cbdyzj <cbdyzj@jianzhao.org>
2019-02-28 21:42:37 +0800

    Update test cases

--- src/test/java/org/jianzhao/jojo/test/JojoTest.java
+++ src/test/java/org/jianzhao/jojo/test/JojoTest.java
@@ -7,45 +7,45 @@ import org.jianzhao.jojo.JsonObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
-@SuppressWarnings("WeakerAccess")
 public class JojoTest {
 
     @Test
-    public void test() {
+    public void test1() {
         Human human = new Human();
         human.setAge(1);
         human.setBirthday(new Date(0));
         human.setName("Ada");
-        human.setHobbies(Stream.of("a", "b", "c").collect(Collectors.toList()));
-        JsonObject entries = JsonObject.mapFrom(human);
-        JsonObject put = new JsonObject()
+        human.setHobbies(Arrays.asList("a", "b", "c"));
+        JsonObject jo1 = JsonObject.mapFrom(human);
+        JsonObject jo2 = new JsonObject()
                 .put("age", 1)
                 .put("name", "Ada")
                 .put("birthday", new Date(0))
                 .put("hobbies", new JsonArray().add("a").add("b").add("c"));
-        boolean equals = put.equals(entries);
+        boolean equals = jo2.equals(jo1);
         Assertions.assertTrue(equals);
     }
 
     @Test
-    public void typeTest() {
+    public void test2() {
         Human human = new Human();
         human.setName("Ada");
         human.setAge(17);
         human.setBirthday(new Date(0));
         human.setHobbies(Arrays.asList("Painting", "Reading"));
 
-        Box<Human> hb = new Box<>();
-        hb.setLabel("human");
-        hb.setData(human);
+        Box<Human> humanBox = new Box<>();
+        humanBox.setLabel("human");
+        humanBox.setData(human);
 
-        List<Box<Human>> boxes = Collections.singletonList(hb);
+        List<Box<Human>> boxes = Collections.singletonList(humanBox);
 
-        List<?> humans = Json.decodeValue(Json.encode(boxes), new TypeReference<List<Box<Human>>>() { });
+        List humans = Json.decodeValue(Json.encode(boxes), new TypeReference<List<Box<Human>>>() { });
         Assertions.assertEquals(1, humans.size());
     }
 }
